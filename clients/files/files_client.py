c860007cd8ce6ca4191c65f31870e6f63665a866
mezail <mezen1sev@gmail.com>
2026-05-17 13:05:45 +0300

    httpx4

--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -5,6 +5,16 @@ from httpx import Response
 from clients.api_client import APIClient
 from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
 
+# Добавили описание структуры файла
+class File(TypedDict):
+    """
+    Описание структуры файла.
+    """
+    id: str
+    url: str
+    filename: str
+    directory: str
+
 class CreateFileRequestDict(TypedDict):
     """
     Описание структуры запроса на создание файла.
@@ -13,6 +23,12 @@ class CreateFileRequestDict(TypedDict):
     directory: str
     upload_file: str
 
+# Добавили описание структуры ответа на создание файла
+class CreateFileResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания файла.
+    """
+    file: File
 
 class FilesClient(APIClient):
     """
@@ -49,6 +65,11 @@ class FilesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/files/{file_id}")
+
+    # Добавили новый метод
+    def create_file(self, request: CreateFileRequestDict) -> CreateFileResponseDict:
+        response = self.create_file_api(request)
+        return response.json()
 
 # Добавляем builder для FilesClient
 def get_files_client(user: AuthenticationUserDict) -> FilesClient:
